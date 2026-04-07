05e0c947479278bb25c0bdc375293abce4dbe433
vova <vovaoleshko05@mail.ru>
2026-04-07 22:46:39 +0300

    cors

--- backend/main.go
+++ backend/main.go
@@ -14,6 +14,7 @@ import (
 	c "github.com/vova1001/krios_proj/config"
 	d "github.com/vova1001/krios_proj/db_init"
 	internal "github.com/vova1001/krios_proj/internal"
+	midlware "github.com/vova1001/krios_proj/midlware"
 )
 
 func main() {
@@ -62,10 +63,11 @@ func main() {
 
 	mux := http.DefaultServeMux
 	handler.RegisterRouter(mux)
+	corsHandler := midlware.CorsMiddleware(mux)
 
 	server := http.Server{
 		Addr:         ":8080",
-		Handler:      mux,
+		Handler:      corsHandler,
 		ReadTimeout:  30 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
